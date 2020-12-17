11cc915730af1c41c4b420741c7c97bc3ccc9d9d
filipspych <filip.spychala@gmail.com>
2020-12-17 21:37:18 +0100

    Clean imports. (subjectivity: 0%)

--- src/cp1/solution/MyManager.java
+++ src/cp1/solution/MyManager.java
@@ -4,7 +4,6 @@ import cp1.base.*;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
